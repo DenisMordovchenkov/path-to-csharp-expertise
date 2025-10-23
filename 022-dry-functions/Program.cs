0e35415cb8c272f4e233d6f5768593a1fe86db35
Denis Mordovchenkov <denisbabichev@gmail.com>
2025-10-23 20:09:27 +0300

    rewrote 022-dry task with methods using

---
+++ 022-dry-functions/Program.cs
@@ -0,0 +1,50 @@
+﻿//Создание числового массива.
+
+int[] CreateArray(int length)
+{
+	return new int[length];
+}
+;
+//Заполнение массива числами от 0 до maxDigit.
+
+void FullfillArray(int[] array, int maxDigit)
+{
+	for (int index = 0; index < array.Length; index++)
+	{
+		array[index] = Random.Shared.Next(maxDigit);
+	}
+}
+;
+//Получение суммы двузначных чисел.
+
+int GetSumOfArray(int[] array)
+{
+	int sum = 0;
+
+	for (int index = 0; index < array.Length; index++)
+	{
+		if (array[index] >= 10 & array[index] <= 99)
+		{
+			sum += array[index];
+		}
+	}
+	return sum;
+}
+
+//Печать в консоль всех элементов массива.
+
+string OutToConsole(int[] array)
+{
+	string output = string.Empty;
+	for (int index = 0; index < array.Length; index++)
+	{
+		output += $" {Convert.ToString(array[index])}";
+	}
+	return output;
+}
+
+int[] array = CreateArray(10);
+FullfillArray(array, 20);
+int sumOfArray = GetSumOfArray(array);
+Console.WriteLine(OutToConsole(array));
+Console.WriteLine(sumOfArray);
